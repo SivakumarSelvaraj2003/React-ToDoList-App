53cb561f69e60b860e19b3bcc2de23e0e91e7219
Sivakumar4568 <sivakumarselvaraj2003@gmail.com>
2025-02-20 08:32:22 -0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/React-ToDoList-App",
+  base:"/React-ToDoList-App/",
   server :{
     port : 3000
   }
